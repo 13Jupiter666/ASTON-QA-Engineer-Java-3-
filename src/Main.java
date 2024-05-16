8bf74f94633208ead38c012b75d9e63f7285651c
13Jupiter666 <saintarnol47@gmail.com>
2024-05-16 21:04:58 +0300

    Lesson_4

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,49 @@
+import java.util.Arrays;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        // 1
         printThreeWords();
+        // 2
         checkSumSign();
+        // 3
         printColor();
+        // 4
         compareNumbers();
+        // 5
         System.out.println(checkSumInRange(5, 6));
+        // 6
         printPositiveOrNegative(7);
+        // 7
         System.out.println(isNegative(-3));
+        // 8
         printStringMultipleTimes("GGWP", 3);
-
-        int[] array1 = createIntArray(10, 5);
-        printArray(array1);
-
+        // 9
+        System.out.println(isLeapYear(2024));
+        // 10
+        int[] array1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        swapArrayElements(array1);
+        System.out.println(Arrays.toString(array1));
+        // 11
         int[] array2 = new int[100];
-        fillArrayWithIncrementingValues(array2);
-        printArray(array2);
-
+        fillArray(array2);
+        System.out.println(Arrays.toString(array2));
+        // 12
         int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        multiplyLessThanSix(array3);
-        printArray(array3);
-
-        int[][] squareArray = createSquareArray(5);
-        printSquareArray(squareArray);
+        multiplyLessThan6(array3);
+        System.out.println(Arrays.toString(array3));
+        // 13
+        int size = 5;
+        int[][] matrix = new int[size][size];
+        fillDiagonal(matrix);
+        printMatrix(matrix);
+        // 14
+        int[] newArray = createArray(3, 6);
+        System.out.println(Arrays.toString(newArray)); // 14
     }
+
 // Задание 1
     public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
@@ -84,47 +104,51 @@ public class Main {
         }
     }
 // Задание 9
-    public static int[] createIntArray(int len, int initialValue) {
-        int[] array = new int[len];
-        for (int i = 0; i < len; i++) {
-            array[i] = initialValue;
-        }
-        return array;
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 // Задание 10
-    public static void fillArrayWithIncrementingValues(int[] array) {
+    public static void swapArrayElements(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] == 0) ? 1 : 0;
+        }
+    }
+
+// Задание 11
+    public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = i + 1;
         }
     }
-// Задание 11
-    public static void multiplyLessThanSix(int[] array) {
+
+// Задание 12
+    public static void multiplyLessThan6(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 6) {
                 array[i] *= 2;
             }
         }
     }
-// Задание 12
-    public static void printArray(int[] array) {
-        for (int value : array) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
+
 // Задание 13
-    public static int[][] createSquareArray(int size) {
-        int[][] squareArray = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            squareArray[i][i] = 1;
+    public static void fillDiagonal(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][i] = 1;
         }
-        return squareArray;
     }
+
 // Задание 14
-    public static void printSquareArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+    public static int[] createArray(int len, int initialValue) {
+        int[] newArray = new int[len];
+        Arrays.fill(newArray, initialValue);
+        return newArray;
+    }
+
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
